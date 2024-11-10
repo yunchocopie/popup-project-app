ba5411168812699165f503db3097554e95e00e7f
yunchocopie <yunnn687@naver.com>
2024-11-10 15:48:34 +0900

    docs : 상세보기 화면 수정

--- lib/ui/holders/home/popup_detail/components/popup_detail_appbar.dart
+++ lib/ui/holders/home/popup_detail/components/popup_detail_appbar.dart
@@ -30,14 +30,14 @@ class PopupDetailAppbar extends StatelessWidget {
         ),
         SizedBox(width: 10),
       ],
-      expandedHeight: screenHeight * 0.5,
+      expandedHeight: screenHeight * 0.45,
       // 사진이 차지하는 화면 비율
       flexibleSpace: PageView.builder(
         itemCount: 1, // 사진 여러개 일시 갯수 입력하고 옆으로 넘기기
         itemBuilder: (context, index) {
           return Image.network(
             // "https://picsum.photos/id/${index + 1}/200/300",
-            'http://tnnews.co.kr/wp-content/uploads/2024/06/%EC%9D%B4%EB%AF%B8%EC%A7%80-03-4%EC%9B%94-%EC%84%B1%EC%88%98%EB%8F%99%EC%97%90%EC%84%9C-%EB%86%93%EC%B9%98%EC%A7%80-%EB%A7%90%EC%95%84%EC%95%BC-%ED%95%A0-%ED%8C%9D%EC%97%85-3%EA%B3%B3-%EC%B6%94%EC%B2%9C_%ED%95%98%EC%9D%B4%ED%8A%B8%EC%A7%84%EB%A1%9C_0412.png',
+            'https://naverbooking-phinf.pstatic.net/20241029_10/1730189320644sTNDW_PNG/PC_02.png?type=a1000_60_sharpen',
             fit: BoxFit.cover,
           );
         },
--- lib/ui/holders/home/popup_detail/components/popup_detail_tabbar_view.dart
+++ lib/ui/holders/home/popup_detail/components/popup_detail_tabbar_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:team_project/ui/holders/home/popup_detail/components/popup_detail_tabbar_reservation.dart';
+import 'package:team_project/ui/holders/home/popup_detail/components/popup_detail_tabbar_reservation_header.dart';
 
 class PopupDetailTabbarView extends StatelessWidget {
   const PopupDetailTabbarView({
@@ -18,13 +21,11 @@ class PopupDetailTabbarView extends StatelessWidget {
                 Row(
                   children: [
                     Text(
-                        '팝업스토어 명',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold
-                        )
+                      '팝업스토어 명',
+                      style: TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
                     ),
-                    SizedBox(width: 15,),
+                    SizedBox(width: 15),
                     Container(
                       width: 45,
                       height: 18,
@@ -33,22 +34,21 @@ class PopupDetailTabbarView extends StatelessWidget {
                         borderRadius: BorderRadius.circular(15),
                         color: Colors.blue,
                       ),
-                      child: Text('상태', style: TextStyle(fontSize: 12, color: Colors.white)),
+                      child: Text(
+                        '상태',
+                        style: TextStyle(fontSize: 12, color: Colors.white),
+                      ),
                     ),
-                    SizedBox(width: 15,),
+                    SizedBox(width: 15),
                     Column(
                       children: [
                         Text(
                           "기간 : ",
-                          style: TextStyle(
-                            fontSize: 10
-                          ),
+                          style: TextStyle(fontSize: 10),
                         ),
                         Text(
-                            "시간 : ",
-                          style: TextStyle(
-                              fontSize: 10
-                          ),
+                          "시간 : ",
+                          style: TextStyle(fontSize: 10),
                         ),
                       ],
                     )
@@ -113,19 +113,10 @@ class PopupDetailTabbarView extends StatelessWidget {
                   height: 1,
                   color: Colors.blue,
                 ),
-                Row(
-                  children: [
-                    Spacer(),
-                    Spacer(),
-                  ],
-                ),
-                // TODO: 지도 적용해야함
               ],
             ),
           ),
-          Center(
-            child: Text('예약'),
-          ),
+          SingleChildScrollView(child: TransactionCalenderPage()),
           Center(
             child: Text('리뷰'),
           ),
