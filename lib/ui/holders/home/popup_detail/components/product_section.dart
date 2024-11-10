597dec326626044b48abcd575bac3b63680afd22
p-jh0 <jhenpark02@donga.ac.kr>
2024-11-10 21:10:26 +0900

    temp

---
+++ lib/ui/holders/home/popup_detail/components/product_section.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ProductSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.all(16.0),
+      children: [
+        _buildProductTile(
+          "https://via.placeholder.com/150",
+          "키링",
+          5500,
+        ),
+        Divider(),
+        _buildProductTile(
+          "https://via.placeholder.com/150",
+          "텀블러",
+          25000,
+        ),
+        Divider(),
+        _buildProductTile(
+          "https://via.placeholder.com/150",
+          "크로스백",
+          45000,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildProductTile(String imageUrl, String productName, int price) {
+    return ListTile(
+      leading: Image.network(
+        imageUrl,
+        width: 60,
+        height: 60,
+        fit: BoxFit.cover,
+      ),
+      title: Text(productName, style: TextStyle(fontWeight: FontWeight.bold)),
+      subtitle: Text("₩$price", style: TextStyle(color: Colors.grey[600])),
+    );
+  }
+}
